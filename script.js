62e2096ecb7916b77892befbbbe95a0c8a5a6ccf
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2017-09-11 14:46:32 -0400

    Append to DOM

--- script.js
+++ script.js
@@ -2,10 +2,28 @@ var searchForm = document.querySelector('form');
 
 searchForm.addEventListener('submit', function(event) {
   event.preventDefault();
+
   var searchTerm = document.querySelector('input').value
-  console.log(searchTerm)
+
   var toLower = searchTerm.toLowerCase()
+
   $.get(`http://pokeapi.co/api/v2/pokemon/${toLower}`, function(data) {
-  console.log(data);
+    // console.log(data.stats[0].stat.name)
+    // console.log(data.abilities[0].ability.name);
+    var name = data.forms[0].name;
+    document.querySelector('#pokeName').innerText = name
+
+    for(let i = 0; i < data.abilities.length; i++) {
+      // console.log(data.abilities[i].ability.name)
+      $('#pokeAbilities').append("<li>"+ data.abilities[i].ability.name+ "</li>")
+    }
+
+    //  let pokeStat = ""
+    for(let i = 0; i < data.stats.length; i++) {
+      console.log(data.stats[i].stat.name)
+      $('#pokeStats').append("<li>" + data.stats[i].stat.name + "</li>")
+    }
+
+
   })
 })
